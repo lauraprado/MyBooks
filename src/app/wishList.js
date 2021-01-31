f19b2ee2fa019808caad5c679751d8943d2efd3b
Laura Prado Marchal <47153839+lauraprado@users.noreply.github.com>
2021-01-31 12:00:34 +0100

    Update wishList.js

--- src/app/wishList.js
+++ src/app/wishList.js
@@ -2,12 +2,15 @@ function createWishlist() {
     clearCards();
     clearSearch();
 
+    const app = document.getElementById('app');
+
     let valor = [];
 
     let divText;
     if (localStorage.length === 0) {
         divText = document.createElement('div');
-        divText.classList.add('element_menu', 'position');
+        divText.setAttribute('id', 'errorWishlist');
+        divText.classList.add('element_error', 'error');
         let text = document.createTextNode('No tienes ningún libro guardado como favorito');
         divText.appendChild(text);
         console.log('vacia');
@@ -16,17 +19,25 @@ function createWishlist() {
             let clave = localStorage.key(i);
             valor.push(localStorage.getItem(clave));
         }
+        console.log('isbn', valor);
+
+        valor.forEach(element => {
+            const url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + element;
+            console.log(url);
+
+            fetch(url)
+                .then(response => response.json())
+                .then(printBooks)
+                .catch(err => {
+                    console.log(err);
+                    let textError = document.createTextNode('Hemos tenido un problema y no podemos mostrarte tu Wishlist');
+                    divText.appendChild(textError);
+                })
+        })
     }
 
-    console.log('isbn', valor);
 
-    valor.forEach(element => {
-        fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:'+ element)
-        .then(response => response.json())
-        .then(printBooks)
-        .catch(err => console.log(err))
-    })
 
     app.appendChild(divText);
-    
+
 }
